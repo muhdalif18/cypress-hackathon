621fc4ce3892725e3373585bf157f9cbcd121a6c
Syed Alif <156448638+muhdalif18@users.noreply.github.com>
2025-07-09 01:07:17 +0800

    report

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // ✅ Reporter config must be at the root
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/results",
-      overwrite: false,
-      html: false,
-      json: true,
-    },
     viewportWidth: 1920,
     viewportHeight: 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "https://my-shop-eight-theta.vercel.app/", // change as needed
+    baseUrl: "https://my-shop-eight-theta.vercel.app/",
     specPattern: "cypress/e2e/**/*.ts",
   },
 });
