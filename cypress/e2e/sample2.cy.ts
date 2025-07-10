f84713165d49013d1d4e36a30d51cb49caa0adb2
Syed Alif <156448638+muhdalif18@users.noreply.github.com>
2025-07-10 09:29:11 +0800

    sample

--- cypress/e2e/sample2.cy.ts
+++ cypress/e2e/sample2.cy.ts
@@ -1,5 +1,5 @@
 describe("Login Functionality - Case Sensitivity Check", () => {
-  it("should login successssssssssfully with correct password and NOT show alert", () => {
+  it("should login successfully with correct password and NOT show alert", () => {
     cy.userLoginHackathon();
 
     cy.url().should("include", "/items.html");
