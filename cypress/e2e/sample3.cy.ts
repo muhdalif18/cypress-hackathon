7f10c807a18958a671bf09c924623af9ce1f917c
Syed Alif <156448638+muhdalif18@users.noreply.github.com>
2025-07-10 15:58:50 +0800

    report

--- cypress/e2e/sample3.cy.ts
+++ cypress/e2e/sample3.cy.ts
@@ -1,4 +1,4 @@
-describe("module2 - Case Sensitivity Check", () => {
+describe("module3 - Case Sensitivity Check", () => {
   const username = Cypress.env("HACKATHON_USER");
   const validPassword = Cypress.env("HACKATHON_VALID_PASS");
   const invalidPassword = Cypress.env("HACKATHON_INVALID_PASS");
---
+++ cypress/e2e/sample4.cy.ts
@@ -0,0 +1,7 @@
+describe("module4 - aasssa", () => {
+  it("should login successfully with correct password and NOT show alert", () => {
+    cy.userLoginHackathon();
+
+    cy.url().should("include", "/items.html");
+  });
+});
