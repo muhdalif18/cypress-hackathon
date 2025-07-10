b4c42bc496fce643b2591c5d65dc45ac69f6e893
Syed Alif <156448638+muhdalif18@users.noreply.github.com>
2025-07-10 15:50:03 +0800

    report

--- cypress/e2e/sample3.cy.ts
+++
@@ -1,8 +0,0 @@
-/* describe("module1 - aaa", () => {
-  it("should login successfully with correct password and NOT show alert", () => {
-    cy.userLoginHackathon();
-
-    cy.url().should("include", "/items.html");
-  });
-});
- */
