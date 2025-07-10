a0a29e52497bc0a554d04a26518f9013e34d3c91
Syed Alif <156448638+muhdalif18@users.noreply.github.com>
2025-07-10 16:21:53 +0800

    report

--- cypress/e2e/sample3.cy.ts
+++ cypress/e2e/sample3.cy.ts
@@ -1,5 +1,5 @@
-describe("HACK004", () => {
-  it("HACK004", () => {
+describe("HACK003", () => {
+  it("HACK003", () => {
     cy.userLoginHackathon();
     cy.wait(1000);
     cy.url().should("include", "/items.html");
